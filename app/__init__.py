3cd060fe4f34b398c9390498e9457b7ad6064e68
guilleJB <contestame@gmail.com>
2016-03-20 23:51:52 +0100

    Register new blueprint on app

--- app/__init__.py
+++ app/__init__.py
@@ -25,5 +25,7 @@ def create_app(config_name):
     # attach routes and custom error pages here
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint, '/auth')
 
     return app
